db7447da2014954d003696b1a87548c900cddd44
aficiomaquinas <victtorglez@gmail.com>
2013-04-27 18:25:10 -0500

    Cambio a express

--- server.js
+++ server.js
@@ -1,6 +1,38 @@
-var connect = require('connect'),
-    http = require('http');
-    
-connect.createServer(
-		connect.static(__dirname)
-).listen(80);
+var express = require('express'),
+	  app = express.createServer()
+
+// Reference
+// https://github.com/vincicat/heroku-express
+// http://expressjs.com/guide.html
+// https://github.com/spadin/simple-express-static-server
+// http://devcenter.heroku.com/articles/node-js
+
+// Configuration
+app.configure(function(){
+	app.use(express.static(__dirname + '/public'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+
+	// app.set('views', __dirname + '/app/views');
+	// app.register('.html', require('jade'));
+
+	// Error Handling
+	app.use(express.logger());
+	app.use(express.errorHandler({
+		dumpExceptions: true, 
+		showStack: true
+	}));
+
+	//Setup the Route, you are almost done
+	app.use(app.router);
+});
+
+app.get('/', function(req, res){
+	res.render("index.html");
+});
+
+// Heroku
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
