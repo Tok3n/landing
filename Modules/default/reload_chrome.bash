d788abdaef0244a38550f95cf190f899ab3c7c26
Victor Gonzalez <aficiomaquinas@users.noreply.github.com>
2015-02-02 22:49:23 -0600

    Remove builds

---
+++ Modules/default/reload_chrome.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+var=$1
+osascript <<EOD
+  set u to "$var"
+  tell application "Chromium"
+    repeat with w in windows
+      set i to 0
+      repeat with t in tabs of w
+        set i to i + 1
+        if URL of t starts with u then
+          set active tab index of w to i
+          set index of w to 1
+          tell t to reload
+          --activate
+          return
+        end if
+      end repeat
+    end repeat
+    open location u
+    --activate
+  end tell
+EOD
